0232f283d3b06896f271d91866dc1373db92f0c5
pmcsh04 <48935917+pmcsh04@users.noreply.github.com>
2020-08-27 10:08:43 +0900

    Add files via upload

---
+++ [2020.08.27]b_7568.py
@@ -0,0 +1,14 @@
+num = int(input())
+
+people = []
+for _ in range(num):
+    w, h = map(int, input().split(" "))
+    people.append((w,h))
+
+for p in people:
+    rank = 1
+    for n in people:
+        if(p[0] != n[0]) & (p[1] != n[1]):
+            if (p[0] < n[0]) & (p[1] < n[1]):
+                rank += 1
+    print(rank, end= ' ')
